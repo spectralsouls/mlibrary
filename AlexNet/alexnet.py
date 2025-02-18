5f26395453c1f68a85a77fbaf5c1523954068592
Igor Gavric <igorg@MacBook-Air-4.local>
2025-02-18 09:23:42 +0100

    training works + final changes

--- AlexNet/alexnet.py
+++ AlexNet/alexnet.py
@@ -7,9 +7,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
-
-# Research Paper: https://sing.stanford.edu/curis-fellowships/rh/vision-dnn.pdf 
-# NOTE: using batch norm instead of their "response-normalization" layers
 # input size: 227x227x3
 
 def sequence(inp, layers): 
@@ -19,9 +16,12 @@ transform = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(22
                                 transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                                              std=[0.229, 0.224, 0.225])])
 
+
+# Download the data here: http://cs231n.stanford.edu/tiny-imagenet-200.zip
+# Expects the data to be in the same directory
+
 train_data = datasets.ImageFolder("./tiny-imagenet-200/train", transform=transform)
-val_data = datasets.ImageFolder("./tiny-imagenet-200/val", transform=transform)
-#test_data = datasets.ImageFolder("./tiny-imagenet-200/test")
+test_data = datasets.ImageFolder("./tiny-imagenet-200/test")
 
 class AlexNet(nn.Module):
     def __init__(self):
@@ -51,7 +51,7 @@ class AlexNet(nn.Module):
             F.relu,
             lambda x: F.dropout(x, 0.5),
             nn.Linear(4096, 1000),
-            lambda x: F.softmax(x, dim=1)
+            #lambda x: F.softmax(x, dim=1) cross entropy in pytorch uses softmax internally
         ]
     def __call__(self, inp): return sequence(inp, self.layers)
 
@@ -59,33 +59,36 @@ class AlexNet(nn.Module):
         params = [layer.parameters() for layer in self.layers if isinstance(layer, nn.Module)]
         p = itertools.chain(*params)
         return p
+    
+    def to_device(self, device='cpu'):
+        for layer in self.layers:
+            layer.to(device) if isinstance(layer, nn.Module) else layer
 
-
-if __name__ == "main":
+if __name__ == "__main__":
     train_ImageNet = DataLoader(train_data, batch_size=128, shuffle=True)
+
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu") # mps only works on MacOS
 
 
 model = AlexNet()
+model.to_device(device)
 optimizer = optim.SGD(model.get_params(), lr=0.01, momentum=0.9, weight_decay=0.0005)
 
-
-train_data = datasets.ImageFolder("./tiny-imagenet-200/train", transform=transform)
-val_data = datasets.ImageFolder("./tiny-imagenet-200/val", transform=transform)
-#test_data = datasets.ImageFolder("./tiny-imagenet-200/test")
-
 def train_step():
+    model.train()
     for x_train, y_train in train_ImageNet:
-        optimizer.zero_grad()
+        x_train, y_train = x_train.to(device), y_train.to(device)
         y_pred = model(x_train)
         loss = F.cross_entropy(y_pred, y_train)
         loss_value = loss
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        return loss_value
+    return loss_value
     
 
+# NOTE: takes a long time to train (~ 10 minutes per step on a Macbook Air M3)
+for i in range(20):
+   loss = train_step()
+   print(loss)
 
-#epochs = 90
-
-#for i in range(epochs):
-    #loss = train_step()
